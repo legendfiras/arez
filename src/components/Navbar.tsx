76b3548dadcadf63f3f4c9078571acbc6754d514
legendfiras <firaskotob508@gmail.com>
2025-12-12 03:23:57 +0200

    Fix logo import to resolve visibility issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import logoImg from '../../public/logo.png';
 
 const links = [
   { label: 'Home', href: '/' },
@@ -17,7 +18,7 @@ export default function Navbar() {
       <div className="nav-inner-full">
         <div className="nav-logo-container">
           <Link href="/" className="nav-logo">
-            <Image src="/logo.png" alt="AREZ REAL ESTATE Logo" width={150} height={60} className="nav-logo-image" />
+            <Image src={logoImg} alt="AREZ REAL ESTATE Logo" width={150} height={60} className="nav-logo-image" />
           </Link>
         </div>
         <nav className="nav-links">
